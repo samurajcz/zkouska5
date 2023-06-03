648d53af8b7356d7a0c620ac2f09360e30c6955b
samurajcz <vlastaburak@seznam.cz>
2023-06-03 14:06:28 +0200

    Ahoj

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Byl jsem zde!");
+        System.out.println("Já taky!");
+
+        // Vytvoř funkci která bude sčítat 2 čísla a vypíše jejich funkci
+
+        // Todo: Vytvoř funkci která bude sčítat 2 čísla a vypíše jejich funkci (pro martina)
     }
 
 
